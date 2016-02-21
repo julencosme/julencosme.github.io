3504e0b041d4c24e6ef885190b987d05fac639cc
Julen Cosme <julen.cosme@gmail.com>
2016-02-21 01:24:14 -0600

    Finish Chapter 5 Exercise

--- chapter-exercise-5/zoom.js
+++ chapter-exercise-5/zoom.js
@@ -1,13 +1,13 @@
-/*    JavaScript 6th Edition
- *    Chapter 5
- *    Chapter case
+/*   JavaScript 6th Edition
+ *   Chapter 5
+ *   Chapter case
 
- *    Photo zoom
- *    Variables and functions
- *    Author: 
- *    Date:   
+ *   Photo zoom
+ *   Variables and functions
+ *   Author: Julen D. Cosme
+ *   Date:   16 February 2016
 
- *    Filename: zoom.js
+ *   Filename: zoom.js
  */
 
 "use strict"; // interpret document contents in JavaScript strict mode
@@ -18,7 +18,24 @@ var figFilename = "images/IMG_0" + photoOrderArray[2] + ".jpg";
 
 /* populate img element and create event listener */
 function pageSetup() {
-   document.getElementsByTagName("img")[0].src = figFilename; // assign filename to img element
+  // assign filename to img element
+  document.getElementsByTagName("img")[0].src = figFilename;
+  createEventListener();
+}
+
+/* close window */
+function closeWin() {
+  window.close();
+}
+
+/* create event listener for close button */
+function createEventListener() {
+  var closeWindowDiv = document.getElementsByTagName("p")[0];
+  if (closeWindowDiv.addEventListener) {
+    closeWindowDiv.addEventListener("click", closeWin, false);
+  } else if (closeWindowDiv.attachEvent) {
+    closeWindowDiv.attachEvent("onclick", closeWin);
+  }
 }
 
 /* add img src value and create event listener when page finishes loading */
