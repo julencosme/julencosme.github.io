3ca725d25c7d159af02c0dc599cc82a8f663798f
Julen Cosme <julen.cosme@gmail.com>
2016-04-10 04:57:27 -0500

    Tweaked Directions scripts

--- chapter-exercise-10/script.js
+++ chapter-exercise-10/script.js
@@ -80,7 +80,7 @@ function loadDirections(string) {
   if (typeof google !== 'object') {
     var script = document.createElement("script");
     // include callback invoking geoTest(), to load only after library has loaded
-    script.src = "https://maps.googleapis.com/maps/api/js? v=3.exp&sensor=true&callback=geoTest";
+    script.src = "https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=true&callback=geoTest";
     document.body.appendChild(script);
   }
 }
