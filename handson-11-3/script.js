de81ae0c22af98f3e8e54ac29e5698a26eb2b317
Julen Cosme <julen.cosme@gmail.com>
2016-04-17 06:57:20 -0500

    Finish Hands-On Project 11-4

--- handson-11-3/script.js
+++ handson-11-3/script.js
@@ -1,6 +1,6 @@
 /*  JavaScript 6th Edition
    Chapter 11
-   Hands-on Project 11-2
+   Hands-On Project 11-3
 
    Author: Julen D. Cosme
    Date:   16 April 2016
@@ -59,6 +59,12 @@ function getQuote() {
   httpRequest.open("get","StockCheck.php?t=" + entry, true);
   httpRequest.send();
   httpRequest.onreadystatechange = displayData;
+
+  // clear any existing timeouts preceding starting new countdown
+  clearTimeout(updateQuote);
+  // refresh every 10 seconds
+  var updateQuote = setTimeout('getQuote()', 10000);
+console.log("Refresh setTimeout() ...")
 }
 
 
@@ -68,49 +74,51 @@ function displayData() {
   // only if server states and statuses do not return errors ...
   if (httpRequest.readyState === 4 && httpRequest.status === 200) {
 
-      // store raw response data
-      var stockResults = httpRequest.responseText;
-      // parse String as Array, specifying comma as separator using RegEx
-      var stockItems = stockResults.split(/,|\"/);
+    // store raw response data
+    var stockResults = httpRequest.responseText;
+    // parse String as Array, specifying comma as separator using RegEx
+    var stockItems = stockResults.split(/,|\"/);
 
-      // decrement through data comprising stockItems[] Array, offsetting for index of zero
-      for (var i = stockItems.length - 1; i >= 0; i--) {
-        if (stockItems[i] === "") {
+    // decrement through data comprising stockItems[] Array, offsetting for index of zero
+    for (var i = stockItems.length - 1; i >= 0; i--) {
+      if (stockItems[i] === "") {
 
-          stockItems.splice(i, 1);
-        }
+        stockItems.splice(i, 1);
       }
+    }
 
-      // store DOM object article
-      var articleEl = document.getElementsByTagName("article");
+    // store DOM object article
+    var articleEl = document.getElementsByTagName("article");
 
-      // write to HTML values derived from parsed/indexed XHR object
-      document.getElementById("ticker").innerHTML = stockItems[0];
-      document.getElementById("openingPrice").innerHTML = stockItems[6];
-      document.getElementById("lastTrade").innerHTML = stockItems[1];
-      document.getElementById("lastTradeDT").innerHTML = stockItems[2] + ", " + stockItems[3];
-      document.getElementById("change").innerHTML = stockItems[4];
+    // write to HTML values derived from parsed/indexed XHR object
+    document.getElementById("ticker").innerHTML = stockItems[0];
+    document.getElementById("openingPrice").innerHTML = stockItems[6];
+    document.getElementById("lastTrade").innerHTML = stockItems[1];
+    document.getElementById("lastTradeDT").innerHTML = stockItems[2] + ", " + stockItems[3];
+    document.getElementById("change").innerHTML = stockItems[4];
 
-      // convert Number to String, using toFixed() to specify decimal place of two; concatenate values
-      document.getElementById("range").innerHTML = (stockItems[8] * 1).toFixed(2) + " &ndash;" + (stockItems[7] * 1).toFixed(2);
-      // convert Number to locale-specific String
-      document.getElementById("volume").innerHTML = (stockItems[9] * 1).toLocaleString();
+    // convert Number to String, using toFixed() to specify decimal place of two; concatenate values
+    document.getElementById("range").innerHTML = (stockItems[8] * 1).toFixed(2) + " &ndash;" + (stockItems[7] * 1).toFixed(2);
+    // convert Number to locale-specific String
+    document.getElementById("volume").innerHTML = (stockItems[9] * 1).toLocaleString();
 
-      // store API URL + user fieldset entry
-      var chartSrc = "http://ichart.yahoo.com/t?s=" + entry;
+    // store API URL + user fieldset entry
+    var chartSrc = "http://ichart.yahoo.com/t?s=" + entry;
 
-      // assign URL from which to fetch chart
-      document.getElementById("chart").src = chartSrc;
-      // assign inline style so as to effect display of chart in DOM
-      document.getElementById("chart").style.display = "inline";
+    // assign URL from which to fetch chart
+    document.getElementById("chart").src = chartSrc;
+    // assign inline style so as to effect display of chart in DOM
+    document.getElementById("chart").style.display = "inline";
   }
 }
 
 
 // script styling of zebra-shaded rows comprising table
 function formatTable() {
+
   // instantiate rows derived from HTML table
   var rows = document.getElementsByTagName("tr");
+
   // iterate alternately through rows; shading every other row comprising table
   for (var i = 0; i < rows.length; i = i + 2) {
     rows[i].style.background = "#9FE098";
