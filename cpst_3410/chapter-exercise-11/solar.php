eab00c70c6980b05810e70d40fd2528f952e8a32
Julen Diaz Cosme <len.diazcosme@gmail.com>
2019-06-29 15:29:51 -0600

    Delete cpst_3410 directory

--- cpst_3410/chapter-exercise-11/solar.php
+++
@@ -1,6 +0,0 @@
-<?php
-$WeatherSource = "https://api.forecast.io/forecast/63b7b74bc3f72a4bc39ce10f3e09a644/" . $_GET["lat"] . "," . $_GET["lng"];
-header("Content-Type: application/json");
-header("Cache-Control: no-cache");
-readfile($WeatherSource);
-?>
