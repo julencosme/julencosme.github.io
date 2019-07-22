b2289230a239402f7707a0af95beeb0ddd5aad20
Julen Diaz Cosme <len.diazcosme@gmail.com>
2019-07-22 10:59:12 -0600

    Modified sass code

--- source/index.js
+++ source/index.js
@@ -1,19 +1,16 @@
+// To use jQuery, first install it as a dependency: `npm install --save jquery`.
+// Then include `import $ from 'jquery';` at the top every JavaScript file that uses jQuery.
+// $('span').mouseover(() => {
+//   alert('span span!');
+// });
 // import $ from 'jquery'; Before using jQuery, install it with `npm install --save jquery`
-import './more-javascript/more.js';
+// import './more-javascript/more.js';
 // import 'normalize.css'; // Note this
-// import './style/style.scss';
-// import './style/jdcWEB.css';
+
+import './style/style.scss';
 
 const saySomething = something => {
   console.log(something); // eslint-disable-line no-console
 };
 
 saySomething('Something! (index.js)');
-
-/*
-To use jQuery, first install it as a dependency: `npm install --save jquery`.
-Then include `import $ from 'jquery';` at the top every JavaScript file that uses jQuery.
-$('span').mouseover(() => {
-  alert('span span!');
-});
-*/
