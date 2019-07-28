d2056856904caf633c0f3710bb21c64ac5e2d288
Julen Diaz Cosme <len.diazcosme@gmail.com>
2019-07-27 20:54:48 -0600

    Revamp image gallery; clean up documentation

--- source/index.js
+++ source/index.js
@@ -1,16 +1,30 @@
-// To use jQuery, first install it as a dependency: `npm install --save jquery`.
-// Then include `import $ from 'jquery';` at the top every JavaScript file that uses jQuery.
-// $('span').mouseover(() => {
-//   alert('span span!');
-// });
-// import $ from 'jquery'; Before using jQuery, install it with `npm install --save jquery`
-// import './more-javascript/more.js';
-// import 'normalize.css'; // Note this
+/**
+ * To use jQuery, first install it as a dependency:
+ *
+ * ```
+ * npm install --save jquery
+ * ```
+ *
+ * Then include:
+ *
+ * ```
+ * import $ from 'jquery';`
+ * ```
+ *
+ * At the top every JavaScript file that uses jQuery.
+ *
+ * To import other files, adhere to the following conventions:
+ *
+ * ```
+ * import './more-javascript/more.js';
+ * import 'normalize.css'; // Note this
+ *
+ * const saySomething = something => {
+ *   console.log(something);
+ * };
+ *
+ * saySomething('Something! (index.js)');
+ * ```
+ */
 
 import './style/style.scss';
-
-const saySomething = something => {
-  console.log(something); // eslint-disable-line no-console
-};
-
-saySomething('Something! (index.js)');
